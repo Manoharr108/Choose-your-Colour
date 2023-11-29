01d473c0520a9cd037fdd2aec6b074f6cb465e75
Manohar R <142599616+Manoharr108@users.noreply.github.com>
2023-11-29 16:21:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+let input = document.getElementById("input");
+let change = document.getElementById("chg");
+let clear = document.getElementById("clr");
+// change.addEventListener("click",(input)=>{
+    //     document.body.style.background = input;
+    //     console.log(input);
+    // })
+    
+function color(){
+    document.body.style.background = input.value;
+    if(input.value == "black"){
+        let head = document.getElementById("heading"). style. color = "white";
+        // head.style.font = "white";
+    }
+    else{
+        let head = document.getElementById("heading"). style. color = "black";
+    }
+
+}
+//
+function damage(){
+
+    input.value = "";
+}
